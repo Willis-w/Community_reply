b0f1bbb0ec9218a8e16a670d7908c7c247e4d864
Willis-w <coastd54703@gmail.com>
2025-02-10 21:09:51 +0800

    Initial commit

--- app.py
+++ app.py
@@ -64,7 +64,7 @@ def callback():
     # get request body as text
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
-    print("Request body: " + body)
+    # print("Request body: " + body)
 
     # parse webhook body
     try:
